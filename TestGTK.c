071c9381d6d6a0fec64d0665c68ff7c45b4e8d8e
Videeki <videeki@gmail.com>
2023-09-24 11:47:42 +0200

    Create a well worked srcolled treeview version

--- TestGTK.c
+++ TestGTK.c
@@ -1,4 +1,11 @@
-// Compiling: gcc `pkg-config --cflags gtk+-3.0` -o Builds/TestGTK TestGTK.c `pkg-config --libs gtk+-3.0`
+/****************************************************************************************************************
+*   Compiling:                                                                                                  *
+*                                                                                                               *
+*    Linux:                                                                                                     *
+*    gcc `pkg-config --cflags gtk+-3.0` -o Builds/TestGTK TestGTK.c `pkg-config --libs gtk+-3.0`                *
+*                                                                                                               *
+****************************************************************************************************************/
+ 
 
 // Standard library
 #include <stdio.h>
@@ -10,15 +17,23 @@
 /* Define the columns of the list */
 enum { NAME_COLUMN, OFFSET_COLUMN, SIZE_COLUMN, N_COLUMNS };
 
+
+struct songTreeView
+{
+    GtkWidget* songView;
+    GtkListStore* songListStore;
+};
+
 /* Create a new GtkListStore and fill it with some data */
 void end_program(GtkWidget *wid, gpointer ptr)
 {
     gtk_main_quit();
 }
 
-GtkListStore* create_list_store()
+GtkListStore* init_list_store(GtkWidget *view)
 {
-    GtkListStore* store; GtkTreeIter iter;
+    GtkListStore* store;
+    GtkTreeIter iter;
 
     store = gtk_list_store_new(N_COLUMNS, G_TYPE_STRING, G_TYPE_INT, G_TYPE_INT);
 
@@ -26,6 +41,56 @@ GtkListStore* create_list_store()
     gtk_list_store_set(store, &iter, NAME_COLUMN, "test name", OFFSET_COLUMN, 0, SIZE_COLUMN, 10, -1);
     gtk_list_store_append(store, &iter);
     gtk_list_store_set(store, &iter, NAME_COLUMN, "another name", OFFSET_COLUMN, 20, SIZE_COLUMN, 15, -1);
+    gtk_list_store_append(store, &iter);
+    gtk_list_store_set(store, &iter, NAME_COLUMN, "test name", OFFSET_COLUMN, 0, SIZE_COLUMN, 10, -1);
+
+    /* Set the model of the tree view to the list store */
+    gtk_tree_view_set_model(GTK_TREE_VIEW(view), GTK_TREE_MODEL(store));
+
+    g_object_unref(store);
+
+    return store;
+}
+
+GtkListStore* previous_list_store(GtkWidget *widget, gpointer data)
+{
+    GtkListStore* store;
+    GtkTreeIter iter;
+
+    store = gtk_list_store_new(N_COLUMNS, G_TYPE_STRING, G_TYPE_INT, G_TYPE_INT);
+
+    gtk_list_store_append(store, &iter);
+    gtk_list_store_set(store, &iter, NAME_COLUMN, "test name", OFFSET_COLUMN, 0, SIZE_COLUMN, 10, -1);
+    gtk_list_store_append(store, &iter);
+    gtk_list_store_set(store, &iter, NAME_COLUMN, "another name", OFFSET_COLUMN, 20, SIZE_COLUMN, 15, -1);
+    gtk_list_store_append(store, &iter);
+    gtk_list_store_set(store, &iter, NAME_COLUMN, "test name", OFFSET_COLUMN, 0, SIZE_COLUMN, 10, -1);
+
+    /* Set the model of the tree view to the list store */
+    gtk_tree_view_set_model(GTK_TREE_VIEW(data), GTK_TREE_MODEL(store));
+
+    g_object_unref(store);
+
+    return store;
+}
+
+GtkListStore* next_list_store(GtkWidget *widget, gpointer data)
+{
+    GtkTreeIter iter;
+    GtkListStore* store;
+    store = gtk_list_store_new(N_COLUMNS, G_TYPE_STRING, G_TYPE_INT, G_TYPE_INT);
+    
+    gtk_list_store_append(store, &iter);
+    gtk_list_store_set(store, &iter, NAME_COLUMN, "test name 2", OFFSET_COLUMN, 42, SIZE_COLUMN, 24, -1);
+    gtk_list_store_append(store, &iter);
+    gtk_list_store_set(store, &iter, NAME_COLUMN, "another name 2", OFFSET_COLUMN, 69, SIZE_COLUMN, 96, -1);
+    gtk_list_store_append(store, &iter);
+    gtk_list_store_set(store, &iter, NAME_COLUMN, "test name 2", OFFSET_COLUMN, 666, SIZE_COLUMN, 666, -1);
+  
+    /* Set the model of the tree view to the list store */
+    gtk_tree_view_set_model(GTK_TREE_VIEW(data), GTK_TREE_MODEL(store));
+
+    g_object_unref(store);
 
     return store;
 }
@@ -51,7 +116,14 @@ GtkWidget* create_tree_view()
 
 int main(int argc, char* argv[])
 { 
-    GtkWidget* window; GtkWidget* view; GtkListStore* store;
+    GtkWidget* window;
+    GtkWidget* vbox;
+    GtkWidget* label;
+    GtkWidget* sw;
+    GtkWidget* button;
+
+    GtkWidget* view;
+    GtkListStore* store;
 
     /* Initialize GTK */
     gtk_init(&argc, &argv);
@@ -59,23 +131,41 @@ int main(int argc, char* argv[])
     /* Create the main window */
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "GTK List Example");
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+    
 
     /* Connect the destroy signal to quit the application */
     g_signal_connect(window, "destroy", G_CALLBACK(end_program), NULL);
 
+    gtk_container_set_border_width(GTK_CONTAINER(window), 8);
+
+    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 8);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    label = gtk_label_new("This is the test list");
+    gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 0);
+    
+    sw = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(sw), GTK_SHADOW_ETCHED_IN);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+    gtk_box_pack_start(GTK_BOX(vbox), sw, TRUE, TRUE, 0);
+
+
     /* Create the list store and the tree view */
-    store = create_list_store();
     view = create_tree_view();
-
-    /* Set the model of the tree view to the list store */
-    gtk_tree_view_set_model(GTK_TREE_VIEW(view), GTK_TREE_MODEL(store));
-
-    /* Unreference the list store to avoid memory leaks */
-    g_object_unref(store);
+    store = init_list_store(view);
 
     /* Add the tree view to the window */
-    gtk_container_add(GTK_CONTAINER(window), view);
+    gtk_container_add(GTK_CONTAINER(sw), view);
+
+    button = gtk_button_new_with_label("Next");
+    g_signal_connect(button, "clicked", G_CALLBACK(next_list_store), view);
+    gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
+
+    button = gtk_button_new_with_label("Previous");
+    g_signal_connect(button, "clicked", G_CALLBACK(previous_list_store), view);
+    gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
+
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 500);
 
     /* Show the window and start the main loop */
     gtk_widget_show_all(window);
